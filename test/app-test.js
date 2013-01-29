403790c544ac9f3ac0736f5586b97529f85153a1
Ruijunhuang <ruijunhuang.2010@sis.smu.edu.sg>
2013-01-30 00:06:46 +0800

    integrate with responsive storyboard page

--- test/app-test.js
+++ test/app-test.js
@@ -22,12 +22,14 @@ if (testing=='true') {
   var myApp = angular.module('myApp', ['ngResource', 'analytics','ngMockE2E']);
   
   myApp.config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('', {templateUrl: 'partials/home.html', controller: IndexController});
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: IndexController});
     $routeProvider.when('/quests', {templateUrl: 'partials/quests.html', controller: IndexController});
     $routeProvider.when('/practice', {templateUrl: 'partials/practice.html', controller: IndexController});
     $routeProvider.when('/create', {templateUrl: 'partials/create.html', controller: IndexController});
     $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: IndexController});
     $routeProvider.when('/teach', {templateUrl: 'partials/teach.html', controller: IndexController});
+    $routeProvider.when('/storyboard', {templateUrl: 'partials/storyboard.html', controller: IndexController});
     $routeProvider.otherwise({redirectTo: 'partials/home.html'});
   }]);
 
