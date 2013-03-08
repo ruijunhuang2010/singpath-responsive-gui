7a62017d07f8b500cbc6df3e49d06eefc1c313ab
Chris Boesch <scboesch@gmail.com>
2013-03-08 10:24:53 +0800

    Before PP Session

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -541,14 +541,128 @@ function GameResultController($scope,$resource){
                 //alert("Found key");
                 $scope.analysis[day].attempts += $scope.items[i].attempts;
                 $scope.analysis[day].solve_time += $scope.items[i].solve_time;
-                
+                $scope.analysis[day].thePlayers[$scope.items[i].player] = 1;
               }
               else{
                 //alert("Did not find key");
                 $scope.analysis[day] = {"attempts": $scope.items[i].attempts, 
-                                        "solve_time":$scope.items[i].solve_time};
+                                        "solve_time":$scope.items[i].solve_time,
+                                        "thePlayers": {}};
               }
-              //$scope.permutation_lines += $scope.game.problems.problems[$scope.current_problem_index].lines[parseInt($scope.permutation[i])-1]+"\n";
+              //Count the number of players
+              var keys = [];
+              for(var key in $scope.analysis[day].thePlayers){
+                keys.push(key);
+              }
+              $scope.analysis[day].players = keys;
+            }
+         
+        };       
+}
+
+function CohortAnalysisController($scope,$resource){
+        //$scope.items = [];
+        //$scope.analysis = {};
+        //$scope.offset = 0;
+        $scope.players_by_join_day = {}; //lists of players
+        $scope.player_events = {}; //list of events
+        $scope.event_count = {};
+        $scope.players_event_count = {};
+        
+        $scope.Player = $resource('/jsonapi/rest/player/:id');
+        $scope.IPUser = $resource('/jsonapi/ipuser/:id');
+                
+
+        $scope.get_players = function(){
+          $scope.Player.query({},
+                function(response) { 
+                  $scope.players = response;
+                  $scope.player_offset = $scope.players.length;
+                  $scope.get_player_events();
+                });  
+        };
+
+        $scope.keys = function(obj){
+          var keys = [];
+          for(var key in obj){
+            keys.push(key);
+          }
+          return keys;
+        };
+        
+        $scope.get_player_events = function(){
+          for (var i = 0; i < $scope.players.length; i++) {
+
+              var join_day = $scope.players[i].created.split('T')[0];
+              if(join_day in $scope.players_by_join_day){
+                //alert("Found key");
+                $scope.players_by_join_day[join_day].push($scope.players[i].id);
+              }
+              else{
+                $scope.players_by_join_day[join_day] = [];
+                $scope.players_by_join_day[join_day].push($scope.players[i].id);
+               }
+              
+              //Fetch the last 100 events for every player
+              $scope.player_events[$scope.players[i].id] = $scope.IPUser.query({'player': $scope.players[i].id});
+              
+              /*
+              $scope.currentPlayerID = $scope.players[i].id;
+              
+              //try{
+                  
+                $scope.IPUser.query({'player': $scope.currentPlayerID},
+                  function(response) { 
+                    alert("Saving for player "+$scope.currentPlayerID);
+                    $scope.player_events[$scope.currentPlayerID] = response;
+                  });
+              //}
+              //catch(err){
+              //  alert("No response for player "+$scope.currentPlayerID);      //Handle errors here
+              //} 
+              */ 
+          
+          }
+        };
+
+
+        $scope.append_list = function(){
+
+          var data = {'model':$scope.model, 'offset':$scope.offset}
+          $scope.Model.query(data,
+                function(response) { 
+                  var temp = response;
+                  $scope.items = $scope.items.concat(temp);
+                  $scope.offset += temp.length;
+                  $scope.analyze();
+                  //alert("items "+$scope.items.length+" offset "+$scope.offset+" length "+$scope.items.length)
+                });  
+        };
+
+        $scope.analyze = function(){
+            $scope.analysis = {};
+            for (var i = 0; i < $scope.items.length; i++) {
+              var day = $scope.items[i].game_start.split('T')[0];
+              //time = $scope.items[i].game_start.split('T')[1];
+              //if ($scope.points.hasOwnProperty(key))
+              if(day in $scope.analysis){
+                //alert("Found key");
+                $scope.analysis[day].attempts += $scope.items[i].attempts;
+                $scope.analysis[day].solve_time += $scope.items[i].solve_time;
+                $scope.analysis[day].thePlayers[$scope.items[i].player] = 1;
+              }
+              else{
+                //alert("Did not find key");
+                $scope.analysis[day] = {"attempts": $scope.items[i].attempts, 
+                                        "solve_time":$scope.items[i].solve_time,
+                                        "thePlayers": {}};
+              }
+              //Count the number of players
+              var keys = [];
+              for(var key in $scope.analysis[day].thePlayers){
+                keys.push(key);
+              }
+              $scope.analysis[day].players = keys;
             }
          
         };       
