22694e873a2f616e049cbe6bb3ed60a5685cb527
LI HE <heli.2010@smu.edu.sg>
2013-04-28 23:28:55 +0800

    updated dnd game with dynamic game creation with questID, reformatted for untidy code

--- app/djs/app.js
+++ app/djs/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //var myApp = angular.module('myApp', ['ngResource', 'analytics']);
-var myApp = angular.module('dnd', ['ngResource','ngCookies','ngResource']);
+var myApp = angular.module('dnd', ['ngResource','ngCookies']);
 
 myApp.run(function($rootScope, $location) {
     $rootScope.location = $location;
