13094bdf3aaee619ae8c7b5508d8922fceb5a463
LI HE <heli.2010@smu.edu.sg>
2013-05-12 00:43:18 +0800

    updated for previous/next button on storyboard video pop-up & back to story board button on normal game playpage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -269,10 +269,10 @@ function NormalGameController($scope,$resource,$cookieStore){
         $scope.move_to_next_unsolved_problem = function(){
           $scope.sampleAnswers = "yes";
           if ($scope.remaining_problems.length>0){
-			$('#t1').addClass('active');
-			$('#t2').removeClass('active');
-			$('#ta1').addClass('active');
-			$('#ta2').removeClass('active');
+      			$('#t1').addClass('active');
+      			$('#t2').removeClass('active');
+      			$('#ta1').addClass('active');
+      			$('#ta2').removeClass('active');
             //Todo:If you are already on the problem, you don't need to reload it. 
             $scope.current_problem = $scope.remaining_problems[$scope.skip_problem_count % $scope.remaining_problems.length];
             $scope.current_problem_index = $scope.game.problemIDs.indexOf($scope.current_problem);
@@ -287,10 +287,10 @@ function NormalGameController($scope,$resource,$cookieStore){
 
         }
         $scope.skip_problem = function(){
-		    $('#t1').addClass('active');
-			$('#t2').removeClass('active');
-			$('#ta1').addClass('active');
-			$('#ta2').removeClass('active');
+  		    $('#t1').addClass('active');
+    			$('#t2').removeClass('active');
+    			$('#ta1').addClass('active');
+    			$('#ta2').removeClass('active');
           if ($scope.remaining_problems.length>1){
             $scope.skip_problem_count += 1;
             $scope.move_to_next_unsolved_problem();
@@ -307,10 +307,10 @@ function NormalGameController($scope,$resource,$cookieStore){
           //$scope.solution
           //$scope.current_problem
           //$scope.game.gameID
-			$('#t1').removeClass('active');
-			$('#t2').addClass('active');
-			$('#ta1').removeClass('active');
-			$('#ta2').addClass('active');
+    			$('#t1').removeClass('active');
+    			$('#t2').addClass('active');
+    			$('#ta1').removeClass('active');
+    			$('#ta2').addClass('active');
           $scope.SaveResource = $resource('/jsonapi/verify_for_game');
           //alert($scope.game.gameID);
           $scope.theData = {user_code:$scope.solution1,
@@ -389,7 +389,15 @@ function NormalGameController($scope,$resource,$cookieStore){
             videos = numOfUnlocked;
           },true);
         };
+<<<<<<< HEAD
 		    
+=======
+
+        $scope.goStoryBoard = function(){
+          window.location = "index.html#/storyboard";
+        };
+		
+>>>>>>> updated for previous/next button on storyboard video pop-up & back to story board button on normal game playpage
 		$scope.create_quest_game($scope.qid);
 }
 
@@ -722,7 +730,10 @@ function QuestController($scope,$resource,$location,$routeParams,$cookieStore){
     $scope.quests = new Array();
     $scope.changeRoute = 'normal_play_page.html';
     $scope.name = $cookieStore.get("name");
-	$scope.storyid = 14611860;
+    if($cookieStore.get("name")){
+      $scope.questID = $cookieStore.get("name").id;//retrieve quest id from Storyboard page
+    }
+	  $scope.storyid = 14611860;
     //Create quest
     $scope.create_quest = function(storyID,pathID,difficulty){
 /*       //alert("storyID "+storyID+" pathID "+ pathID+" difficult "+difficulty);
@@ -817,6 +828,14 @@ function QuestController($scope,$resource,$location,$routeParams,$cookieStore){
       $location.path('storyboard');
     };
 
+    $scope.updateQuest = function(){
+     $resource('/jsonapi/quest/:questID').get({"questID":$scope.questID},
+        function(response){
+          $scope.name = response;
+          $cookieStore.put("name", $scope.name);
+     });
+    };
+
     $scope.$watch('name', function() {
       if($scope.name && $scope.name.difficulty == "Drag-n-Drop"){
         $scope.changeRoute = "playPage.html";
@@ -824,6 +843,7 @@ function QuestController($scope,$resource,$location,$routeParams,$cookieStore){
     }, true);
 
     $scope.list();
+    $scope.updateQuest();
 
 }
 
